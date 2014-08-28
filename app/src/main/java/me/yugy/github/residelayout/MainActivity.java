0ff1b89c17cde9feda940d50a1bb189026b4ee05
kyze8439690 <me@yanghui.name>
2014-08-28 19:48:08 +0800

    repo init

---
+++ app/src/main/java/me/yugy/github/residelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package me.yugy.github.residelayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
